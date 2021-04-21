ab66f95aa14a18c056f790c87de0844702b5d593
leitongchen <leitongchen93@gmail.com>
2021-04-21 17:13:06 +0200

    commenti

---
+++ myscript.js
@@ -0,0 +1,24 @@
+/* si esegue un alert che contiene 5 numeri da memorizzare
+ */ 
+
+
+/* quando utente clicca ok
+    parte il setTimeout di 30 secondi 
+ */ 
+
+
+
+// FUNZIONE che crea numeri random e li pusha in un array 
+
+
+/* Trascorsi 30 secondi compare prompt
+    in cui l'utente reinserisce i 5 numeri random che ha visualizzato
+*/ 
+// Inserisco prompt dentro un ciclo così compare finché l'utente non ha inserito 5 valori
+    // I valori vengono pushati dentro un array userNumbers
+
+
+// FUNZIONE che verifica che i numeri inseriti dall'utente corrispondano a quelli del primo alert
+    // i numeri corretti vengono inseriti in una nuova array correctNumbers
+
+
