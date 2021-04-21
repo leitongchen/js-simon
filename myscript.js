e235bbce19514f812b9672d3f9aab7322664dae5
leitongchen <leitongchen93@gmail.com>
2021-04-21 17:32:12 +0200

    Numeri random: f e array

--- myscript.js
+++ myscript.js
@@ -1,24 +1,52 @@
-/* si esegue un alert che contiene 5 numeri da memorizzare
- */ 
+function randomNumGen(maxNum) {
+
+    var randomNum = Math.ceil(Math.random() * maxNum); 
+
+    return randomNum;
+}
+
+(function () {
+
+    // FUNZIONE che crea numeri random e li pusha in un array 
+    var fiveNumbers = []; 
+ 
+    while (fiveNumbers.length < 5) {
+
+        var randomNumber = randomNumGen(50);
+        fiveNumbers.push(randomNumber); 
+    }
+
+    console.log(fiveNumbers);
 
 
-/* quando utente clicca ok
-    parte il setTimeout di 30 secondi 
- */ 
+
+
+    /* si esegue un alert che contiene 5 numeri da memorizzare
+     */
+    alert(fiveNumbers.join(", ") + " - Memorizza questi 5 numeri, quando sei pronto, premi OK.");
+
+    /* quando utente clicca ok
+        parte il setTimeout di 30 secondi 
+     */
 
 
 
-// FUNZIONE che crea numeri random e li pusha in un array 
 
 
-/* Trascorsi 30 secondi compare prompt
-    in cui l'utente reinserisce i 5 numeri random che ha visualizzato
-*/ 
-// Inserisco prompt dentro un ciclo così compare finché l'utente non ha inserito 5 valori
+    /* Trascorsi 30 secondi compare prompt
+        in cui l'utente reinserisce i 5 numeri random che ha visualizzato
+    */
+    // Inserisco prompt dentro un ciclo così compare finché l'utente non ha inserito 5 valori
     // I valori vengono pushati dentro un array userNumbers
 
 
-// FUNZIONE che verifica che i numeri inseriti dall'utente corrispondano a quelli del primo alert
+    // FUNZIONE che verifica che i numeri inseriti dall'utente corrispondano a quelli del primo alert
     // i numeri corretti vengono inseriti in una nuova array correctNumbers
+
+
+
+
+})();
+
 
 
