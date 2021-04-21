570c00ba70e7d072d76e662520ba90c6431393fc
leitongchen <leitongchen93@gmail.com>
2021-04-21 20:06:45 +0200

    controllo su userInput

--- myscript.js
+++ myscript.js
@@ -29,28 +29,34 @@ function checkTheNumber(numbersArray, numToCheck) {
     return true; 
 }
 
+function inputIsValid(input){
+    if (isNaN(input) || input < 1 || input > 100) {
+        return false;
+    }
+    return true;
+}
+
 
 (function () {
 
     // FUNZIONE che crea numeri random e li pusha in un array 
     var fiveNumbers = [];
+    var userNumbers = [];
+    var guessedNumbers = [];
+
 
     while (fiveNumbers.length < 5) {
 
-        var randomNumber = randomNumGen(1000);
+        var randomNumber = randomNumGen(100);
         if (checkForDouble(fiveNumbers, randomNumber) === -1) {
 
             fiveNumbers.push(randomNumber);
         }
     } 
 
-    console.log(fiveNumbers);
-
-
-
     /* si esegue un alert che contiene 5 numeri da memorizzare
      */
-    alert(fiveNumbers.join(", ") + " - Memorizza questi 5 numeri, quando sei prontə, premi OK.");
+    alert(fiveNumbers.join(", ") + " - Memorizza questi numeri, quando sei prontə premi OK.");
 
     /* quando utente clicca ok
         parte il setTimeout di 30 secondi 
@@ -59,22 +65,21 @@ function checkTheNumber(numbersArray, numToCheck) {
     /* Trascorsi 30 secondi compare prompt
     in cui l'utente reinserisce i 5 numeri random che ha visualizzato
     */
-    setTimeout(numbersMemory, 5000);
+    setTimeout(numbersMemory, 30000);
 
     function numbersMemory() {
 
         // Inserisco prompt dentro un ciclo così compare finché l'utente non ha inserito 5 valori
         // I valori vengono pushati dentro un array userNumbers
 
-        var userNumbers = [];
-
-        var guessedNumbers = [];
-
         while (userNumbers.length < 5) {
 
             var userInput = parseInt(prompt("Ora reinserisci i 5 numeri che hai memorizzato. Ne hai inseriti " + userNumbers.length))
 
-            if (checkForDouble(userNumbers, userInput) === -1) {
+            if (!inputIsValid(userInput)) {
+                alert("Non hai inserito un valore valido. Puoi inserire solo numeri a partire da 1 a 100.")
+            
+            } else if (checkForDouble(userNumbers, userInput) === -1) {
                 
                 userNumbers.push(userInput);
                 
@@ -88,11 +93,10 @@ function checkTheNumber(numbersArray, numToCheck) {
                 alert("Hai già inserito questo numero.")
             }
         }
-
+        console.log("Questi erano i numeri che dovevi memorizzare " + fiveNumbers.join(" - "));
+        console.log(fiveNumbers)
         console.log("Il gioco è finito. Questi sono i numeri che hai inserito " + userNumbers.join(" - ") + ".");
         console.log("Questi sono i numeri corretti che hai inserito " + guessedNumbers.join(" - ") + ". Ne hai indovinati " + guessedNumbers.length + ".");
-        console.log("Questi erano i numeri che dovevi memorizzare " + fiveNumbers.join(" - "))
-
     }
 
 
